d9c24ef5f609827534d67889bda9fae545831715
WillForan <willforan@gmail.com>
2025-01-31 12:38:39 -0500

    00_update_wiki_snr.bash: module load for crc

--- helpers/00_update_wiki_snr.bash
+++ helpers/00_update_wiki_snr.bash
@@ -1,5 +1,6 @@
 cd "$(dirname "$0")"
-source ../.venv/bin/activate
+[[ $(uname -a) =~ crc ]] && module load python/ondemand-jupyter-python3.11
+source ../.venv/bin/activate # python3 -m venv ../.venv
 source .creds
 # python3 -m pip install -r ./requirements.txt
 # DRYRUN=1 ./run_all_mrrcqa.py
