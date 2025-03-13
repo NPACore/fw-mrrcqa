4c09c9273ca3248daf03c3dc432682ef523db7ea
WillForan <willforan@gmail.com>
2025-03-12 21:28:40 -0400

    🎨 Program/QC.m cleanup

--- Program/QC.m
+++ Program/QC.m
@@ -4,21 +4,31 @@
 #
 
 addpath(fileparts(mfilename('fullpath')));
+
+%% handle input arguments
 dicom_dir = argv{1};
 if length(argv) < 2
   output_dir = 'outputs';
 else
   output_dir = argv{2};
 end
+
+%% Main
+dcm_stats = dostat(dicom_dir, 0);
+
+%% Write ouputs
 % NB. will not make recursive output dir?
 if ~exist(output_dir,'dir'), mkdir(output_dir); end
 
-dcm_stats = dostat(dicom_dir, 0);
-fid = fopen(fullfile(output_dir, 'stats.json'),'w');
+json_outfile = fullfile(output_dir, 'stats.json');
+
+fprintf('saving to %s\n', json_outfile);
+fid = fopen(json_outfile,'w');
 %dcm_stats_write = rmfield(dcm_stats,'bufstr');
 fwrite(fid, jsonencode(dcm_stats));
 fclose(fid);
 
+image_outfile = fullfile(output_dir, 'bars.png');
 f = figure('visible','off');
 plotQC(dcm_stats,'', f);
-saveas(f, fullfile(output_dir, 'bars.png'));
+saveas(f, image_outfile);
