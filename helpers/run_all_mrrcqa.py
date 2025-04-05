d2833e7e00d22d85ad8e3d2c7ddc3b493769d996
WillForan <willforan@gmail.com>
2025-04-04 21:57:26 -0400

    ✨ run_all_mrrcqa.py: rerun when version is too old

--- helpers/run_all_mrrcqa.py
+++ helpers/run_all_mrrcqa.py
@@ -17,7 +17,7 @@ mrrcqa_gear = fw.lookup("gears/mrrcqa")
 def run_mrrcqa(f: flywheel.models.file_output.FileOutput):
     """Configure and run MRRCQA gear"""
     config = {"write_db": True}
-    tags = ["mrrcqa"]
+    tags = ["mrrcqa", "hpc"]
     inputs = {'phantom_dicom': f}
     job_id = mrrcqa_gear.run(tags=tags, config=config, inputs=inputs, destination=f)
     return job_id
@@ -29,13 +29,23 @@ for f in files:
     acq = fw.get(f.parents['acquisition'])
     ses = fw.get(f.parents['session'])
 
-    # can skip if we've already run the gear and have it's output saved
-    if 'stats.json' in [x.name for x in acq.files]:
-        print(f"# {ses.label} {acq.label} has stats.json")
-        continue
-    if ses.info.get('snr'):
-        print(f"# {ses.label} has stats.json")
-        continue
+    # can skip if a sufficnetly new gear has been run
+    try:
+        stats_idx = [x.name for x in acq.files].index('stats.json')
+        version = acq.files[stats_idx].gear_info.version
+        if version.split('.')[2] >= '20250301':
+            print(f"# SKIP! {fw.get(ses.parents.project).label}/{ses.label} acq='{acq.label}' has version {version}")
+            continue
+        print(f"# version too old: {fw.get(ses.parents.project).label}/{ses.label} acq='{acq.label}' {version}")
+    except ValueError:
+        pass
+
+    #if 'stats.json' in [x.name for x in acq.files]:
+    #    print(f"# {ses.label} {acq.label} has stats.json")
+    #    continue
+    #if ses.info.get('snr'):
+    #    print(f"# {ses.label} has stats.json")
+    #    continue
 
     print(f"running for {ses.subject.code} {ses.label} {f.name}")
     if not DRYRUN:
