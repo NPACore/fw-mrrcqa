c4a425272a8c28388f66e069f503777bb68720be
WillForan <willforan@gmail.com>
2025-04-12 13:24:51 -0400

    📝 docs and wiki_upload to class

---
+++ helpers/readme.md
@@ -0,0 +1,29 @@
+# MRRC QA Helpers
+This directory contains auxiliary scripts to facilitate cleanup and export of the QC gear and data. 
+The scripts interface with Flywheel and the dokuwiki website.
+
+`00_update_wiki_snr.bash` is the main entry point that calls `plots.R`, used in cron like:
+
+```
+0  8 *   *   *   /raidzeus/src/fw-mrrcqa/helpers/00_update_wiki_snr.bash
+```
+
+`plots.R` uses
+  1.  `snr_from_db.py` to query the flywheel database for all snr measures 
+     * values are written by `../Program/run.py` when run as a flywheel gear.
+     * values are pulled using the [Flywheel Python SDK](https://flywheel-io.gitlab.io/product/backend/sdk/) and assume previous authentication with the [CLI](https://docs.flywheel.io/CLI/) (`fw login`)
+  2. ggplot to create a visually summary and highlight outliers over time on the 3 Prisma scanenrs , especially `Z` shim and `tsnr` values
+  3. `wiki_upload.py` to upload the plot to https://rad.pitt.edu/wiki/doku.php?id=scan
+     * over [XML-RPC](https://www.dokuwiki.org/devel:xmlrpc)
+     * credentials pulled from env (`WIKIUSER`/`WIKIPASS`) or [`pass`](https://www.passwordstore.org/)
+
+| script | desc |
+| --- | --- |
+| `00_update_wiki_snr.bash` | setup env for cron run on Zeus nightly |
+| `plots.R`                 | plot SNR, uses `reticulate` to import `snr_from_db.py` and `wiki_upload.py` |
+| `run_all_mrrcqa.py`       | rerun gear on all outdated  |
+| `snr_from_db.py`          | `SNR` class |
+| `wiki_upload.py`          |  `DokuWiki` class, `upload_snr`. uses XML-RPC interface |
+| | |
+| `updatedb.py`             | used once -- write session info with gear output. this is now done by the gear |
+| `wiki_http.py`            | wiki interaction via http POST. Deprecated in favor of `wiki_upload.py` |
--- helpers/wiki_upload.py
+++ helpers/wiki_upload.py
@@ -5,6 +5,7 @@ sftp upload is complicated by UPMC vs Pitt firewall.
 Dokuwiki image upload is one alternative, but must be run from a whitelisted IP (eg. zeus or cerebro).
 
 Other alternatives include wordpress (rad.pitt.edu) drupal/pantheon (mrrc.pitt.edu) or confluence.
+sftp would be best, but we are not on the same network. The firewall blocks access.
 
 Here we're using dokuwiki's XMLRPC interface.
 This must be explicitly enabled and the connecting user must be whitelisted in dokuwiki's admin interface.
@@ -25,49 +26,106 @@ from xmlrpc.client import ServerProxy, Binary
 import os
 import subprocess
 import logging
+from typing import Optional
 
-def upload_image(img_path: str, user: str, password: str, wiki_name=None, wiki_url = 'http://rad.pitt.edu/wiki/'):
-    """
-    Upload an image to a dokuwiki server using XML-RPC (must be enabled for user on wiki side).
-    For more robust XMLRPC interaction, see the python-dokuwiki module on pypi
-    :param img_path: file location of image to upload
-    :param user: wiki user name
-    :param password: matching password
-    :param wiki_name: what to call the upload on the wiki (default is basename of img_path)
-    :param wiki_url: root url of wiki. Trailing slash important
-    """
-    auth =  "Basic " + base64.b64encode(f'{user}:{password}'.encode('utf-8')).decode()
-    header = [("Authorization", auth)]
-    proxy = ServerProxy(wiki_url + "lib/exe/xmlrpc.php", headers=header)
-    
-    logging.info(f"wiki log in test for {user}")
-    login = proxy.dokuwiki.login(user, password)
-    assert login
-    
-    with open(img_path,'rb') as img:
-        img_data=img.read()
 
-    if wiki_name is None:
-        wiki_name = os.path.basename(img_path)
-    
-    logging.info("sending image '%s' as '%s'", img_path, wiki_name)
-    # base64.b64encode(img_data).decode('utf-8')
-    # pitt EWI F5/ASM blocked gives:
-    #   ssl.SSLEOFError: EOF occurred in violation of protocol (_ssl.c:2393)
-    res = proxy.wiki.putAttachment(wiki_name, Binary(img_data), {'ow':True})
-    return res
+class DokuWiki:
+    """
+    Class to hold th authenticated XML-RPC connection object: py:var:`DokuWiki.proxy`
+    """
+
+    user = None
+    password = None
+    wiki_url = ""
+    proxy = None
+
+    def __init__(
+        self, wiki_url: str, user: Optional[str] = None, password: Optional[str] = None
+    ):
+        """
+        :param wiki_url: root url of wiki. Trailing slash important
+        :param user: wiki user name
+        :param password: matching password
+        """
+        self.wiki_url = wiki_url
+        self.set_creds(user, password)  # set user and password
+        self.login()  # set `proxy` XML-RPC interface
+
+    def set_creds(self, user, password):
+        """
+        set credentials if not already defined
+        use environment variables: WIKIUSER and/or WIKIPASS
+        or try to get from 'pass' command
+        :sideffect: will maybe update self.user and self.password
+                    and will raise an Exception if they are not set
+        """
+        if not self.user:
+            self.user = os.environ.get("WIKIUSER", "npac")
+        if not self.password:
+            self.password = (
+                os.environ.get("WIKIPASS")
+                or subprocess.run(["pass", "wiki/npac"], capture_output=True)
+                .stdout.decode()
+                .strip()
+            )
+        # error if still not set
+        # logging.debug("using password: %s", password)
+        assert self.user
+        assert self.password
+
+    def login(self):
+        """
+        login to dokuwiki site. expects user and password likely set by set_creds
+        :sideeffect: sets `proxy` as XML-RPC interface
+        """
+        creds = f"{self.user}:{self.password}"
+        auth = "Basic " + base64.b64encode(creds.encode("utf-8")).decode()
+        header = [("Authorization", auth)]
+        self.proxy = ServerProxy(self.wiki_url + "lib/exe/xmlrpc.php", headers=header)
+
+        logging.info(f"wiki log in test for {self.user}")
+        login = self.proxy.dokuwiki.login(self.user, self.password)
+        assert login
+
+    def upload_file(self, local_path: str, wiki_name=None, binary=True):
+        """
+        Upload an image to a dokuwiki server using XML-RPC (must be enabled for user on wiki side).
+        For more robust XMLRPC interaction, see the python-dokuwiki module on pypi
+        :param local_path: file location of file (image) to upload
+        :param wiki_name: what to call the upload on the wiki (default is basename of local_path)
+        """
+
+        read_spec = "rb" if binary else "r"
+        with open(local_path, read_spec) as fh:
+            file_data = fh.read()
+
+        if wiki_name is None:
+            wiki_name = os.path.basename(local_path)
+
+        logging.info("sending file '%s' as '%s'", local_path, wiki_name)
+        # base64.b64encode(file_data).decode('utf-8')
+        # pitt EWI F5/ASM blocked gives:
+        #   ssl.SSLEOFError: EOF occurred in violation of protocol (_ssl.c:2393)
+        if binary:
+            data = Binary(file_data)
+        else:
+            data = base64.b64encode(file_data).decode("utf-8")
+
+        # ow set to overwrite existing file on wiki side
+        res = self.proxy.wiki.putAttachment(wiki_name, data, {"ow": True})
+        return res
+
 
 def upload_snr(img_path: str):
     """
-    Wrap py:func:`upload_image` to specifically upload SNR.
+    Wrap py:func:`DokuWiki.upload_file` to specifically upload SNR.
+    username and password are pulled from py:func:`set_creds` defaults (npac w/'pass' cmd)
     :param img_path: path of image to upload. likely a temporary file. See snr_from_db.py
     """
 
-    password = os.environ.get('WIKIPASS') or subprocess.run(['pass', 'wiki/npac'],capture_output=True).stdout.decode().strip()
-    #logging.debug("using password: %s", password)
+    dw = DokuWiki("http://rad.pitt.edu/wiki/")
+    return dw.upload_file(img_path, wiki_name="mrrc_prismas_snr.png")
 
-    user = os.environ.get('WIKIUSER', 'npac')
-    return upload_image(img_path, user, password, wiki_name='mrrc_prismas_snr.png')
 
 if __name__ == "__main__":
-    upload_snr('/tmp/snr.png')
+    upload_snr("/tmp/snr.png")
