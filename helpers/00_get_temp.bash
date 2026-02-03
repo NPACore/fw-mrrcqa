b83190bcb4b907292d0832ea059d7e721d668646
WillForan <willforan@gmail.com>
2026-02-03 13:55:59 -0500

    doc(temp): annotate rsync

--- helpers/00_get_temp.bash
+++ helpers/00_get_temp.bash
@@ -5,3 +5,7 @@ export SSHPASS=$(pass gyrus2)
 [ -z "$SSHPASS" ] && echo "cannot find gyrus2 password!" && exit 1
 sshpass -e scp gyrus2:"/raidgyrus2/TWIX/temp\ log.xlsx" tempurature_log.xlsx
 chmod a-x tempurature_log.xlsx
+
+# alternative: 
+#   rsync -av gyrus2:"/raidgyrus2/TWIX/temp log.xlsx" tempurature_log.xlsx
+# will preserve timestamp. but want to know this was updated on this end?
