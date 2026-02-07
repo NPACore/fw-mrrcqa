172a932854d9aeee6d1102932b815071dab176e9
WillForan <willforan@gmail.com>
2026-02-07 08:40:13 -0500

    fix(plots): keep "X" column, not "X.1" rowname; work on reese guix

--- helpers/00_update_wiki_snr.bash
+++ helpers/00_update_wiki_snr.bash
@@ -39,6 +39,9 @@ case "$(uname -a)" in
    guix shell \
      glibc@2.39 r r-ggrepel r-pacman r-reticulate r-dplyr r-tidyr r-ggplot2 r-lubridate r-lubridate r-cowplot r-readxl -- \
      ./plots.R;;
+  reese*)
+    # already ran 'guix shell --pure coreutils python@3.11 -- sh -c 'python3 -m venv .venv-3.11 &&  source .venv-3.11/bin/activate && pip install seaborn flywheel-sdk'
+    guix shell coreutils python@3.11 r r-ggrepel r-pacman r-reticulate r-dplyr r-tidyr r-ggplot2 r-lubridate r-lubridate r-cowplot r-readxl --pure -- Rscript plots.R;;
  *) # *crc*
    [[ $(uname -a) =~ crc ]] && module load python/ondemand-jupyter-python3.11 r
    source ../.venv/bin/activate # python3 -m venv ../.venv
