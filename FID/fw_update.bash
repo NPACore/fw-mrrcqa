ca09e6fa3f3d765f99ea7aed86dfa374d5a70a81
WillForan <willforan@gmail.com>
2026-03-16 16:18:00 -0400

    feat(fw_update): script options for file to upload

--- FID/fw_update.bash
+++ FID/fw_update.bash
@@ -11,14 +11,18 @@ getfwtime(){
    date --utc -d "$REMOTE_TIME" +"%s"
 }
 
-for scanner in Prisma1QA Prisma2QA Prisma3QA; do
- for file in fwhm.py coil.py; do
-   ltime=$(getftime $file)
-   rtime=$(getfwtime $scanner $file)
+[[ $* =~ ^-h ]] && echo "USAGE: $0 [all|fwhm.py coil.py ...]" && exit 0
+[[ $* =~ '^all' ]] && FILES=(fwhm.py coil.py) || FILES=("$@")
+
+for file in "${FILES[@]}"; do
+  ! test -r "$file" && echo "missing '$file', no file to upload!" && continue
+  for scanner in Prisma1QA Prisma2QA Prisma3QA; do
+   ltime=$(getftime "$file")
+   rtime=$(getfwtime $scanner "$file")
    diff=$(perl -e "print $ltime - $rtime")
-   echo "# $file: l=$ltime $(date -d "@$ltime" +"%F %T"); r=$rtime $(date -d "@$rtime" +"%F %T") |  $diff"
+   echo "# $scanner/$file: l=$ltime $(date -d "@$ltime" +"%F %T"); r=$rtime $(date -d "@$rtime" +"%F %T") |  $diff"
    [ "$diff" -gt 120 ] &&
-     dryrun fw upload $file fw://mrrc/$scanner ||
+     dryrun fw upload "$file" fw://mrrc/$scanner ||
      echo "# not uploading $file, not new enough"
   done
 done
