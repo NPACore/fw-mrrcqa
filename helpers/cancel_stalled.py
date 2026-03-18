f6012343aec1681df3907375638456adb20c8707
WillForan <willforan@gmail.com>
2026-03-18 14:14:33 -0400

    fix: cancel job using correct var; add JOBAGE_SECONDS

--- helpers/cancel_stalled.py
+++ helpers/cancel_stalled.py
@@ -2,15 +2,25 @@
 """
 Cancel stalled jobs. likely b/c HPC is busted.
 20250617: 'srun' not installed on some nodes (fixed with warewulf image update)
+20260305: add environ variables; switch back to run instead of mod time (always within 8 seconds?!)
+          here becuaes 84/~900 fw-mrrcqa rerun failed in slurm but didn't cancel job?
+JOBAGE_SECONDS=3000 VERBOSE=1 DRYRUN=1 ./cancel_stalled.py
 """
+import os
 import flywheel
 from datetime import datetime
+VERBOSE = os.environ.get("VERBOSE")
+
 def older_than_day(rundate: datetime) -> bool:
     """
     :param rundate: time to compare to now
     :return: True if time is older than one day
     """
-    return (datetime.now(tz=rundate.tzinfo) - rundate).seconds > 24*60*60
+    max_age = int(os.environ.get("JOBAGE_SECONDS", 24*60*60))  # 86400
+    current_age = (datetime.now(tz=rundate.tzinfo) - rundate).total_seconds()
+    if VERBOSE:
+        print(f"current age: {current_age}")
+    return current_age > max_age
 
 if __name__ == "__main__":
     fw = flywheel.Client()
@@ -18,13 +28,15 @@ if __name__ == "__main__":
     #[x.update(state='cancelled') for x in running if older_than_day(x.transitions['running'].tzinfo)]
     
     for job in running:
-        #rundate = job.transitions['running']
-        rundate = job.modified
+        rundate = job.transitions['running']
+        #rundate = job.modified
+        if VERBOSE:
+            print(job.transitions)
         if not older_than_day(rundate):
             stat = "[continue]"
         elif os.environ.get("DRYRUN",False):
             stat="[DRYRUN skip cancelling]"
         else:
             stat="[CANCEL]"
-            x.update(state='cancelled')
+            job.update(state='cancelled')
         print(f"{stat} {job.id} modified last on {rundate}")
