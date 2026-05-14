f653ad36a860bdccc7215251cb96a55b0ee9ea3e
WillForan <willforan@gmail.com>
2026-05-14 17:53:16 -0400

    WIP: spline interpolate
    Port MATLAB spline to python for flywheel/file-curator.
    Interpolation after FFT so FWHM isn't identical every time.
    
    Co-author: Chan-Hong Moon <moonc@upmc.edu>

--- FID/fwhm.py
+++ FID/fwhm.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """
-Check scanner B0.
-Use FWHM on FFT of free induction decay QA sequence
+Check Shimming (B0).
+Extract power spectrum's FWHM from FFT on free induction decay.
 
-Can also be used as a file-curator flywheel gear.
+Can also be used as a file-curator flywheel gear
+ FW file-curator runs Curator.curate_file from gear/gear rule
 
 20260316 - default db name is 'fwhm'
            But want to run for single voxel spec sequence too ('fwhm_svs')
+20260514 - spline interpolation for SVS. optional env var INTERP_FAC (not flywheel)
+           default to genderating FWHM with 10x if too few samples
 """
 
 import numpy as np
@@ -14,8 +17,10 @@ import pydicom
 import re
 import logging
 import sys
+import os
 from zipfile import ZipFile
 from typing import Any, Dict
+from scipy.interpolate import UnivariateSpline
 
 # Import coil module - try from current directory first, then from additional input
 try:
@@ -36,21 +41,44 @@ except ImportError:
             pass
 
 
-def fid_fwhm(dcm, plot=True):
-    """Full Width Half Max of Free Inducation decay"""
+def fid_fwhm(dcm, plot=True, interp_fac=1):
+    """Full Width Half Max of Free Inducation decay
+    @param dcm pydicom object
+    @param plot show a plot
+    @param interp_fac factor to inc number of samples by (1 is disabled. want 10x for SVS)
+    """
     csa = coil.csareader.read(dcm[(0x0029, 0x1110)].value)
-    dt = (
-        coil.read_item(csa, "RealDwellTime") or 0
-    ) * 1e-9  # sec; scanner ADC sampling time
+
+    # scanner ADC sampling time converted to seconds
+    dt = (coil.read_item(csa, "RealDwellTime") or 0) * 1e-9
     bw = 1 / dt  # Hz
     # t = rng*dt
     fftfid = coil.fft_signal(dcm)
+
     absfft = np.abs(fftfid)
 
     nt = absfft.shape[0]  # coil.read_item(csa, 'DataPointRows') or 0
     rng = np.arange(0, nt)
     df = bw / nt
     f = (rng - nt / 2) * df  # frequency
+
+    # 20260514 - spline interpolate after FFT
+    # SVS power spectrum too sparse, FWHM always returns same value
+    # more (interpolated) samples to
+    # from CHM, matlab like
+    #        abs_fftfidint = spline(f,abs(fftfid),[f(1):df/10:f(end)]);
+    # TODO: get actual length
+    if len(absfft) <= 1024 and interp_fac == 1:
+        logging.warning(
+            (
+                f"FFT has too few elements for FWHM ({len(absfft)}.",
+                "Using spline to add samples.",
+            )
+        )
+        interp_fac = 10
+    if interp_fac > 1:
+        spline = UnivariateSpline(f, absfft)
+        absfft = spline(np.arange(f[0], f[-1], df / interp_fac))
 
     half_max = np.max(absfft) / 2
     above_hm = f[absfft - half_max > 0]
@@ -176,20 +204,27 @@ class Curator(FileCurator):
         # Handle both zip files and direct DICOM files
         file_path = file_["location"]["path"]
         dcm = first_dicom_from_zip(file_path)
+        acq_id = file_["hierarchy"]["id"]
+
+        # NOTE: will set interp_fac to 10 for SVS automatically
         fwhm = fid_fwhm(dcm, plot=False)
         print(f"FID FWHM\t{fwhm:2.3f}\t{file_path}")
 
         # Update flywheel database with FWHM value
-        acq_id = file_["hierarchy"]["id"]
         update_fwhm_stat(acq_id, fwhm, self.client)
 
         return fwhm
 
 
 if __name__ == "__main__":
-    import sys
+
+    #: environment variable FWHM_INTERPFAC. not run if 1 (default)
+    INTERP_FAC = int(os.environ.get("FWHM_INTERPFAC", "1"))
+    if len(sys.argv) <= 1:
+        print("ERROR: No input arguments. Proivde a or list of dicom files")
+        sys.exit(1)
 
     for dcm_file in sys.argv[1:]:
         dcm = pydicom.dcmread(dcm_file)
-        fwhm = fid_fwhm(dcm)
+        fwhm = fid_fwhm(dcm, interp_fac=INTERP_FAC)
         print(f"FID FWHM\t{fwhm:2.3f}\t{dcm_file}\n")
